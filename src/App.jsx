b6acd121d50ab1dd56ed764d048b6b7cc72089eb
Mohammad Alom <malom1@fordham.edu>
2025-03-21 18:46:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Information from './Information'
+
+function App() {
+
+  return (
+    <>
+    <Information></Information>
+    </>
+  )
+}
+
+export default App
---
+++ src/Information.jsx
@@ -0,0 +1,9 @@
+function Information () {
+    return(
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Information
