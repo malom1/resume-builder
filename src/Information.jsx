6497c088b4b1f9a91a79ccb24137af0f232bbf1c
Mohammad Alom <malom1@fordham.edu>
2025-03-22 17:04:46 -0400

    add setter function to set values and add to console as an object

--- src/Information.jsx
+++ src/Information.jsx
@@ -2,28 +2,57 @@ import { useState } from "react"
 
 function Information () {
 
+    const [value, setValue] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        location: '',
+        school: '',
+        major: '',
+        company: '',
+        description: ''
+    })
+
+    const handleChanges = (e) => {
+
+        setValue({...value, [e.target.name]:[e.target.value]})
+
+    }
+    
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        console.log(value)
+    }
+
     return (
         <div className="container">
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="personal-info">
 
                     <h1>Personal Information</h1>
 
                     <label htmlFor="firstname">First Name</label>
-                    <input type="text" name="firstname" placeholder="Enter First Name"/>
+                    <input type="text" name="firstname" placeholder="Enter First Name"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="lastname">Last Name</label>
-                    <input type="text" name="firstname" placeholder="Enter Lirst Name"/>
+                    <input type="text" name="lastname" placeholder="Enter Last Name"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="email">Email</label>
-                    <input type="mail" name="email" placeholder="Enter Email"/>
+                    <input type="mail" name="email" placeholder="Enter Email"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="phone">Phone Number</label>
-                    <input type="text" name="phone" placeholder="Enter Phone Number"/>
+                    <input type="text" name="phone" placeholder="Enter Phone Number"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="location">Location</label>
-                    <input type="text" name="location" placeholder="City, State"/>
+                    <input type="text" name="location" placeholder="City, State"
+                    onChange={(e) => handleChanges(e)}/>
 
                 </div>
 
@@ -32,13 +61,15 @@ function Information () {
                     <h1>Education</h1>
 
                     <label htmlFor="school">School</label>
-                    <input type="text" name="school" placeholder="Enter School Name"/>
+                    <input type="text" name="school" placeholder="Enter School Name"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="major">Major</label>
-                    <input type="text" name="major" placeholder="Enter Major"/>
-
+                    <input type="text" name="major" placeholder="Enter Major"
+                    onChange={(e) => handleChanges(e)}/>
+{/* 
                     <label htmlFor="minor">Minor</label>
-                    <input type="text" name="minor" placeholder="Enter Minor"/>
+                    <input type="text" name="minor" placeholder="Enter Minor"/> */}
 
                     <label htmlFor="start-date">Start Date</label>
                     <input type="date" name="start-date"/>
@@ -53,10 +84,12 @@ function Information () {
                     <h1>Work Experience</h1>
 
                     <label htmlFor="company">Company</label>
-                    <input type="text" name="company" placeholder="Enter Company Name"/>
+                    <input type="text" name="company" placeholder="Enter Company Name"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="location">Location</label>
-                    <input type="text" name="location" placeholder="City, State"/>
+                    <input type="text" name="location" placeholder="City, State"
+                    onChange={(e) => handleChanges(e)}/>
 
                     <label htmlFor="start-date">Start Date</label>
                     <input type="date" name="start-date"/>
@@ -65,11 +98,11 @@ function Information () {
                     <input type="date" name="end-date"/>
 
                     <label htmlFor="description">Description</label>
-                    <textarea name="description"></textarea>
+                    <textarea name="description" onChange={(e) => handleChanges(e)}></textarea>
 
                 </div>
 
-                
+                <button>Submit</button>
 
             </form>
 
