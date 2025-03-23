ad887564e0f62dec322e6e7269f2912ae19a7ef0
Mohammad Alom <malom1@fordham.edu>
2025-03-23 16:31:37 -0400

    add resume preview component and pass props through app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
-import Information from './Information'
+import Information from './Components/Information'
+import ResumePreview from './Components/ResumePreview'
+import { useState } from 'react'
 
 function App() {
 
+  const [resumeData, setResumeData] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    homeLocation: '',
+    school: '',
+    major: '',
+    schoolStart: '',
+    schoolEnd: '',
+    company: '',
+    position: '',
+    workStart: '',
+    workEnd: '',
+    workLocation: '',
+    description: ''
+})
+
   return (
     <>
-    <Information></Information>
+    <Information resumeData={resumeData} setResumeData={setResumeData}/>
+    <ResumePreview resumeData={resumeData}/>
     </>
   )
 }
---
+++ src/Components/Information.jsx
@@ -0,0 +1,161 @@
+
+function Information ({resumeData, setResumeData}) {
+
+    const handleChanges = (e) => {
+
+        setResumeData({...resumeData, [e.target.name]: e.target.value})
+
+    }
+    
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        console.log(resumeData)
+    }
+
+    return (
+        <div className="container">
+
+            <form onSubmit={handleSubmit}>
+                <div className="personal-info">
+
+                    <h1>Personal Information</h1>
+
+                    <label htmlFor="firstname">First Name</label>
+                    <input 
+                        type="text" 
+                        name="firstname" 
+                        placeholder="Enter First Name"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="lastname">Last Name</label>
+                    <input 
+                        type="text" 
+                        name="lastname" 
+                        placeholder="Enter Last Name"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="mail" 
+                        name="email" 
+                        placeholder="Enter Email"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="phone">Phone Number</label>
+                    <input 
+                        type="text" 
+                        name="phone" 
+                        placeholder="Enter Phone Number"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="homeLocation">Location</label>
+                    <input 
+                        type="text" 
+                        name="location" 
+                        placeholder="City, State"
+                        onChange={handleChanges}
+                    />
+
+                </div>
+
+                <div className="education">
+
+                    <h1>Education</h1>
+
+                    <label htmlFor="school">School</label>
+                    <input 
+                        type="text" 
+                        name="school" 
+                        placeholder="Enter School Name"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="major">Major</label>
+                    <input 
+                        type="text" 
+                        name="major" 
+                        placeholder="Enter Major"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="schoolStart">Start Date</label>
+                    <input 
+                        type="date" 
+                        name="schoolStart"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="schoolEnd">End Date</label>
+                    <input 
+                        type="date" 
+                        name="schoolEnd"
+                        onChange={handleChanges}
+                    />
+
+                </div>
+
+                <div className="experience">
+
+                    <h1>Work Experience</h1>
+
+                    <label htmlFor="company">Company</label>
+                    <input 
+                        type="text" 
+                        name="company" 
+                        placeholder="Enter Company Name"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="position">Position</label>
+                    <input 
+                        type="text" 
+                        name="position" 
+                        placeholder="Enter Position"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="workLocation">Location</label>
+                    <input 
+                        type="text" 
+                        name="workLocation" 
+                        placeholder="City, State"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="workStart">Start Date</label>
+                    <input 
+                        type="date" 
+                        name="workStart"
+                        onChange={handleChanges}/>
+
+                    <label htmlFor="workEnd">End Date</label>
+                    <input 
+                        type="date" 
+                        name="workEnd"
+                        onChange={handleChanges}
+                    />
+
+                    <label htmlFor="description">Description</label>
+                    <textarea 
+                        name="description" 
+                        placeholder="Responsibilities"
+                        onChange={handleChanges}>
+                    </textarea>
+
+                </div>
+
+                <button>Submit</button>
+
+            </form>
+
+        </div>
+
+    )
+}
+
+export default Information
