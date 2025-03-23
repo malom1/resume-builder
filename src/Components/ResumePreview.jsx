8cb893cf593efafe7987b003d51a70338b473fca
Mohammad Alom <malom1@fordham.edu>
2025-03-23 16:51:58 -0400

    display resume information as state

--- src/Components/ResumePreview.jsx
+++ src/Components/ResumePreview.jsx
@@ -1,5 +1,21 @@
-export default function ResumePreview() {
+export default function ResumePreview({resumeData}) {
 
-    
+    return(
+        <div className="resume-preview">
+            <h1>{resumeData.firstname} {resumeData.lastname}</h1>
+            <p>{resumeData.email} | {resumeData.phone} | {resumeData.homeLocation}</p>
+
+            <h2>Education</h2>
+            <p><strong>{resumeData.school}</strong></p>
+            <p>{resumeData.major}, {resumeData.schoolStart} - {resumeData.schoolEnd}</p>
+
+            <h2>Professional Experience</h2>
+            <p><strong>{resumeData.company}</strong>, {resumeData.workLocation}</p>
+            <p>{resumeData.position}, {resumeData.workStart} - {resumeData.workEnd}</p>
+            <p>{resumeData.description}</p>
+
+        </div>
+    )
+
 
 }
